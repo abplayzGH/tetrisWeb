32f49d7e7b8e179ea479cae1bc8f8661ddba9a54
signal86 <signal86@github.com>
2024-02-21 12:58:38 -0600

    add: game class

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
+let game;
 
-// 300 x 150
-// Boxes 30 x 10
+// 634 x 919
+// Boxes 63 x 63
 
 class Piece {
     
@@ -22,15 +23,34 @@ class Square extends Piece {
 
 }
 
-function prepare() {
+class Game {
 
-    ctx.fillStyle = "green";
-    ctx.fillRect(0, 0, 200, 100);
-    // ctx.beginPath();
-    // ctx.moveTo(0, 0);
-    // ctx.lineTo(30, 0);
-    // ctx.stroke();
+    constructor() {  }
+
+    prepare() {
+
+        ctx.fillStyle = "black";
+        ctx.fillRect(0, 0, 634, 919);
+        ctx.fillStyle = "black";
+        ctx.fillRect(0, 882, 634, 37);
+        ctx.strokeStyle = "white";
+        ctx.lineWidth = 4;
+        for (let i = 2; i < 630; i += 63) {
+            for (let v = 2; v < 882; v += 63) {
+                ctx.strokeRect(i, v, 63, 63);
+            }
+        }
+    
+    }
+
+    loop() {
+
+        this.prepare();
+
+    }
 
 }
 
-prepare();
+
+game = new Game();
+game.loop();
